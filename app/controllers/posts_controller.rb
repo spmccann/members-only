90a5447a2c0fde330e1d864b38e65c7527f3a3c4
Scott <scottpmccann@gmail.com>
2022-08-20 14:06:51 -0700

    author and post body are hidden unlesslogged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:show]
 
   # GET /posts or /posts.json
   def index
